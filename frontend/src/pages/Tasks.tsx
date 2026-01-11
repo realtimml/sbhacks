bbef7ad8c9df61c0e6339f474b04f72d2019b793
realtimml <timmyphan066@gmail.com>
2026-01-10 19:54:25 -0800

    Make header and divider for tasks page

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -1,7 +1,16 @@
 function Tasks() {
   return (
-    <div className="tasks-page">
-      {/* Tasks page content will be implemented later */}
+    <div className="flex flex-col h-full p-6">
+      {/* Header */}
+      <h1 className="font-serif text-3xl text-[#393939]">Tasks</h1>
+      
+      {/* Divider */}
+      <div className="w-full h-px bg-[#C5BDAD] mt-4 mb-6" />
+      
+      {/* Task items area - to be implemented later */}
+      <div className="flex-1 overflow-y-auto">
+        {/* Task items will go here */}
+      </div>
     </div>
   )
 }
